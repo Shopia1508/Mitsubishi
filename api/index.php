c3058d7a07282dbcc8a39dcd87c9717316d6d69a
shopia <shopiaamalia152@gmail.com>
2026-07-10 14:25:09 +0700

    fix vercel deep bootstrap integration

--- api/index.php
+++ api/index.php
@@ -1,2 +1,15 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+// Paksa memuat autoloader vendor secara rapi
+require __DIR__ . '/../vendor/autoload.php';
+
+// Paksa mendaftarkan bootstrap engine agar class [view] otomatis terbaca
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Jalankan aplikasi melalui kernel web global
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
